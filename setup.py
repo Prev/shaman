760bee84667dfa9f01ceba1b44ffcb311e20588c
Prev <prevdev@gmail.com>
2020-07-26 15:52:46 +0900

    Update setup.py to upload long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
 	name = 'shamanld',
+	long_description=long_description,
+    long_description_content_type='text/markdown',
 	packages = ['shamanld'],
-	version = '1.0.1',
+	version = '1.0.2',
 	description = 'Programming Language Detector',
 	license = 'MIT',
 
